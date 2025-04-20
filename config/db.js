ae610b62076301f562d3d214e398ec5f99642b79
Yash Bisaria <yashbisaria@Yashs-MacBook-Air.local>
2025-04-20 17:14:33 +0530

    trouble shooting

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://guptatejasv86086:IHiNa09sVQaCgM9N@cluster0.6etxq2u.mongodb.net/InvertisCare_db?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://guptatejasv86086:IHiNa09sVQaCgM9N@cluster0.6etxq2u.mongodb.net/samadhaan_db?retryWrites=true&w=majority&appName=Cluster0"
     );
 
     console.log(" MONGO DB CONNECTED ");
