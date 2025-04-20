011ad0e4ad7512e9d82e10b4413f766cd7841002
Yash Bisaria <yashbisaria@Yashs-MacBook-Air.local>
2025-04-20 17:35:27 +0530

    trouble shooting

--- models/user.js
+++ models/user.js
@@ -1,44 +1,7 @@
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema( //{
-  // username: { type: String, required: true, unique: true },
-  // password: { type: String, required: true },
-  //}
-  {
-    email: {
-      type: String,
-      //required: true,
-      unique: true,
-      match: [/.+\@.+\..+/, "Please enter a valid email"],
-    },
-    password: {
-      type: String,
-      //required: true,
-      select: false,
-    },
-    phone: {
-      type: String,
-    },
-    course: {
-      type: String,
-    },
-    year: {
-      type: String,
-    },
-    dob: {
-      type: Date,
-    },
-    photo: {
-      type: String,
-      default: "default.jpg",
-    },
-    name: {
-      type: String,
-    },
-  },
-  {
-    timestamps: true,
-    versionKey: false,
-  }
-);
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 module.exports = mongoose.model("user", UserSchema);
 //
