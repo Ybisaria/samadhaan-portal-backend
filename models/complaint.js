457fcb4968855c5dc94ea931e416a9c815b7997c
Yash Bisaria <yashbisaria@Yashs-MacBook-Air.local>
2025-04-21 09:17:38 +0530

    trouble shooting

--- models/complaint.js
+++ models/complaint.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const ComplaintSchema = new mongoose.Schema(
   {
     studentRefId: {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
     subject: {
